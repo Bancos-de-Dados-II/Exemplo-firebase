11a2c415654b2719f606fb4a653dc03d0b52e7f8
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-09-11 14:19:00 -0300

    Adicionando método para fazer login

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import {} from 'dotenv/config';
 
 const firebaseConfig = {
@@ -27,4 +27,20 @@ async function criarUsuario(email, password){
     });
 }
 
-criarUsuario('f.freitas@ifpb.edu.br', '123456');
+// criarUsuario('f.freitas@ifpb.edu.br', '123456');
+
+async function fazerLogin(email, password){
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  });
+}
+
+fazerLogin('f.freitas@ifpb.edu.br', '123456');
